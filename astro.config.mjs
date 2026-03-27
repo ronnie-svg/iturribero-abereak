7ce5fe6d019ee5d39c8b63ac3eda76058ef90153
Izaskun Fernandez <izaskun@spain.zwilling.local>
2026-03-27 15:38:56 +0100

    Fix deploy static Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://iturriberoabereak.com',
   integrations: [sitemap()],
-  output: 'server',   // 🔥 MUY IMPORTANTE
-  adapter: vercel(),  // 🔥 MUY IMPORTANTE
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
